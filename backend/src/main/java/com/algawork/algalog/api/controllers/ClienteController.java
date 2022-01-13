2c8bf4b597767a1234225fac0fb237268e031aec
Rafaelxndre <rafaelxndre@gmail.com>
2022-01-13 15:21:47 -0300

    CRUD de cliente

--- backend/src/main/java/com/algawork/algalog/api/controllers/ClienteController.java
+++ backend/src/main/java/com/algawork/algalog/api/controllers/ClienteController.java
@@ -1,31 +1,71 @@
 package com.algawork.algalog.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algawork.algalog.domain.model.Cliente;
+import com.algawork.algalog.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+	@Autowired
+	private ClienteRepository repository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar(){
-	Cliente cliente1 = new Cliente();
-	cliente1.setId(1L);
-	cliente1.setNome("Rafael");
-	cliente1.setTelefone("11 94827-9871");
-	cliente1.setEmail("rafael@gmail.com");
-	
-	Cliente cliente2 = new Cliente();
-	cliente2.setId(2L);
-	cliente2.setNome("Gabriel");
-	cliente2.setTelefone("11 92748-7198");
-	cliente2.setEmail("gabriel@gmail.com");
-	
-	return Arrays.asList(cliente1, cliente2);
+		return repository.findAll();
 	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar (@PathVariable Long clienteId) {
+		return repository.findById(clienteId)
+				//.map(cliente -> ResponseEntity.ok(cliente))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return repository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		if (!repository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = repository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if (!repository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		repository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
+		}
 
 }
