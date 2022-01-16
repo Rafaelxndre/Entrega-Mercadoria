2752f6915cfba8c6e4e506f55c3c30b94d85b0fa
Rafaelxndre <rafaelxndre@gmail.com>
2022-01-16 15:28:00 -0300

    Validação em cascata e validation Groups

--- backend/src/main/java/com/algawork/algalog/api/controllers/EntregaController.java
+++ backend/src/main/java/com/algawork/algalog/api/controllers/EntregaController.java
@@ -1,7 +1,14 @@
 package com.algawork.algalog.api.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algawork.algalog.domain.model.Entrega;
+import com.algawork.algalog.domain.repository.EntregaRepository;
 import com.algawork.algalog.domain.services.SolicitacaoEntregaService;
 
 @RestController
@@ -16,11 +24,27 @@ import com.algawork.algalog.domain.services.SolicitacaoEntregaService;
 public class EntregaController {
 
 	@Autowired
+	private EntregaRepository entregaRepository;
+	
+	@Autowired
 	private SolicitacaoEntregaService solicitacaoEntregaService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega) {
+	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		return solicitacaoEntregaService.solicitar(entrega);
+	}
+	
+	@GetMapping
+	public List<Entrega> listar(){
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
 	}
 }
--- backend/src/main/java/com/algawork/algalog/domain/model/Entrega.java
+++ backend/src/main/java/com/algawork/algalog/domain/model/Entrega.java
@@ -11,6 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
+
+import com.algawork.algalog.domain.ValidationGroups;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 public class Entrega {
@@ -19,17 +27,30 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;	
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
+	
+	@NotNull
 	private BigDecimal taxa;
 	
+	@JsonProperty(access = Access.READ_ONLY)
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
+	
+	@JsonProperty(access = Access.READ_ONLY)
 	private LocalDateTime dataPedido;
+	
+	@JsonProperty(access = Access.READ_ONLY)
 	private LocalDateTime dataFinalizacao;
+	
 	public Long getId() {
 		return id;
 	}
